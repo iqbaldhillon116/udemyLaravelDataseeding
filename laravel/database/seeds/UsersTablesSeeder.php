e1710117f43594e18c9704ab52cfce8d263e7d93
iqbal <dhillon.iqbal.116@gmail.com>
2021-06-01 00:17:51 +0530

    udemy laravel section 28

---
+++ laravel/database/seeds/UsersTablesSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\db;
+use Illuminate\Support\str;
+class UsersTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+
+            'name'=>Str::random(40),
+            'email'=>Str::random(10).'@gmail.com',
+            'password'=>bcrypt('secret')
+
+
+        ]);
+    }
+}
